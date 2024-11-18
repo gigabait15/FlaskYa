62a0f2f427c92c5847df2017fc5adf5865ca38ce
gigabait15 <Workkerbecov@yandex.ru>
2024-11-18 19:04:00 +0300

    переписал логику роутера входа(проверка в бд по имени пользователя и проверка пароля); роутер для выхода (добавлен декоратор, для доступа только авторизированому пользователю0

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from .models import User
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .forms import LoginForm
+from flask_login import LoginManager, logout_user, login_required
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,10 +22,30 @@ migrate = Migrate(app, db)
 from src import models
 
 
+@app.route('/index')
+def index():
+    return {'Hello': 'World'}
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
