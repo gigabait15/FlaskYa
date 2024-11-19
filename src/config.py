d2cfd42e4f9c62484f0b27ced5842078a52cd2af
gigabait15 <Workkerbecov@yandex.ru>
2024-11-19 11:47:12 +0300

    перенесл некоторые настройки приложения, для избежаний циклических импортов

--- src/config.py
+++ src/config.py
@@ -1,10 +1,27 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:5432"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost/FlaskYa"
 
 
+app = Flask(__name__)
+
+app.config.from_object(Config)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+app.config['SECRET_KEY'] = Config.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_COOKIE_SECURE'] = False
